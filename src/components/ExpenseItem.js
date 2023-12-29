484a5e74fc484cdb13f0d7e1b4b4d67049e74ebb
nabankur2000m <nabankur2000m@gmail.com>
2023-12-30 01:53:50 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Expense Items</div>
+      <div>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
