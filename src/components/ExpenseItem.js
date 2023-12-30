6fff087af1ecd83f6c3870470b78dca73d6d3b85
nabankur2000m <nabankur2000m@gmail.com>
2023-12-30 21:38:27 +0530

    a

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props) {
-
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <div className="expense=item__location">{props.location}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        amount={props.amount}
+        location={props.location}
+      />
     </div>
   );
 }
