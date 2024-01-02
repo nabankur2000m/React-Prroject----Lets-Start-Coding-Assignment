85c95d072a934d07450317e694523e8819d6fa13
nabankur2000m <nabankur2000m@gmail.com>
2024-01-02 21:25:24 +0530

    Adding Event Listeners made easy

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const clickHandler = () => {
+    console.log('Clicked!!!!!!!!!!!');
+  };
+
+  const deleteHandler = () => {
+    setIsDeleted(true);
+  };
+
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-    </Card>
+    <div>
+      {!isDeleted && (
+        <Card className='expense-item'>
+          <ExpenseDate date={props.date} />
+          <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+          </div>
+          <button onClick={clickHandler}>Change Title</button>
+          <button onClick={deleteHandler}>Delete Expense</button>
+        </Card>
+      )}
+    </div>
   );
-}
+};
 
 export default ExpenseItem;
