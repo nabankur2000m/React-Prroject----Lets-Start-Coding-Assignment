4b1fd536c7eefe493d00db494c88f348580087f7
nabankur2000m <nabankur2000m@gmail.com>
2024-01-07 20:45:26 +0530

    Creating your own first Component 1

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,32 +1,24 @@
-import React, { useState } from 'react';
+import React,{ useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount); 
   console.log('ExpenseItem evaluated by React');
 
-  const clickHandler = () => {
+  const clickHandler = () =>{
     setTitle('Updated!');
     console.log(title);
   }
-
-  const changeAmountHandler = () => {
-    setAmount(100);
-    console.log(amount);
-  }
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
-      <button onClick={changeAmountHandler}>Change Amount</button> {}
+      <button onClick={clickHandler}>Change Ttile</button>
     </Card>
   );
 }
