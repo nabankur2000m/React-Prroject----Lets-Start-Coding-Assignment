a492f5164c163e3bc833350bf9568fc75bb1beda
nabankur2000m <nabankur2000m@gmail.com>
2024-01-07 20:25:47 +0530

    Manipulating the DOM - React States

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,31 +4,31 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [isDeleted, setIsDeleted] = useState(false);
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount); 
+  console.log('ExpenseItem evaluated by React');
 
   const clickHandler = () => {
-    console.log('Clicked!!!!!!!!!!!');
-  };
+    setTitle('Updated!');
+    console.log(title);
+  }
 
-  const deleteHandler = () => {
-    setIsDeleted(true);
-  };
+  const changeAmountHandler = () => {
+    setAmount(100);
+    console.log(amount);
+  }
 
   return (
-    <div>
-      {!isDeleted && (
-        <Card className='expense-item'>
-          <ExpenseDate date={props.date} />
-          <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-          </div>
-          <button onClick={clickHandler}>Change Title</button>
-          <button onClick={deleteHandler}>Delete Expense</button>
-        </Card>
-      )}
-    </div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={changeAmountHandler}>Change Amount</button> {}
+    </Card>
   );
-};
+}
 
 export default ExpenseItem;
